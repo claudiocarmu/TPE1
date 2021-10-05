dd7c36ac4b33966aa175895278b0d967b0a84de6
Claudio Carmusciano <claudiocarmu@gmail.com>
2021-10-04 22:29:33 -0300

    le borré el punto y la barra al directorio del router

--- router.php
+++ router.php
@@ -1,5 +1,5 @@
 <?php
-    require_once './controllers/productos.controller.php';
+    require_once 'controllers/productos.controller.php';
 
     define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
